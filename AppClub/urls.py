5232fcc18b51b0f0a7f5f602fdd793925322cfc8
FabrizioPinciroli <pinciroli.fabrizio@gmail.com>
2023-10-03 19:41:06 -0300

    se completo el CRUD en todos los modelos

--- AppClub/urls.py
+++ AppClub/urls.py
@@ -3,41 +3,31 @@ from AppClub.views import *
 urlpatterns = [
 
     path('', inicio, name="Inicio"),
-    path('socio/', socio, name="Socio"),
-    path('deporte/', deporte, name="Deporte"),
-    path('evento/', evento, name="Evento"),
-    path('lista-socios/', listar_socios, name="listaSocios"),
-
-    path('socio-Formulario/', socioFormulario, name="socioFormulario"),
-    path('busqueda-apellido/', busquedaApellido, name="busquedaApellido"),
-    path('buscar/', buscar1, name="buscar1"),
-    path('listaSocios/', listaSocios, name="listaSocios"),
-    path('eliminarSocios/<int:id>', eliminarSocio, name="eliminarSocios"),
-    path('editarSocios/<int:id>', editar_socio, name="editarSocios"),
 
     # vistas basadas en clases, editar.
-    path('listarSocios/', SocioList.as_view, name="listarSocios"),
-    path('detalleSocios/<pk>', SocioDetail.as_view, name="detallarSocios"),
-    path('crearSocios/', SocioCreate.as_view, name="crearSocios"),
-    path('actualizarSocios/<pk>', SocioUpdate.as_view, name="actualizarSocios"),
-    path('deletearSocios/<pk>', SocioDelete.as_view, name="deletearSocios"),
+    path('listarSocios/', SocioList.as_view(), name="listarSocios"),
+    path('detalleSocios/<pk>', SocioDetail.as_view(), name="detallarSocios"),
+    path('crearSocios/', SocioCreate.as_view(), name="crearSocios"),
+    path('actualizarSocios/<pk>', SocioUpdate.as_view(), name="actualizarSocios"),
+    path('eliminarSocios/<pk>', SocioDelete.as_view(), name="eliminarSocios"),
+
+
+    path('listarDeportes/', DeporteList.as_view(), name="listarDeportes"),
+    path('detalleDeportes/<pk>', DeporteDetail.as_view(), name="detallarDeportes"),
+    path('crearDeportes/', DeporteCreate.as_view(), name="crearDeportes"),
+    path('actualizarDeportes/<pk>', DeporteUpdate.as_view(),
+         name="actualizarDeportes"),
+    path('eliminarDeportes/<pk>', DeporteDelete.as_view(), name="eliminarDeportes"),
 
 
 
+    path('listarEventos/', EventoList.as_view(), name="listarEventos"),
+    path('detalleEventos/<pk>', EventoDetail.as_view(), name="detallarEventos"),
+    path('crearEventos/', EventoCreate.as_view(), name="crearEventos"),
+    path('actualizarEventos/<pk>', EventoUpdate.as_view(),
+         name="actualizarEvento"),
+    path('eliminarEventos/<pk>', EventoDelete.as_view(), name="eliminarEventos"),
 
-    path('deporte-Formulario/', deporteFormulario, name="deporteFormulario"),
-    path('busqueda-deporte/', busquedaDeporte, name="busquedaDeporte"),
-    path('buscar2/', buscar2, name="buscar2"),
-    path('listaDeportes/', listaDeportes, name="listaDeportes"),
-    path('eliminarDeportes/<int:id>', eliminarDeporte, name="eliminarDeportes"),
-    path('editarDeportes/<int:id>', editar_deporte, name="editarDeportes"),
-
-    path('evento-Formulario/', eventoFormulario, name="eventoFormulario"),
-    path('busqueda-evento/', busquedaEvento, name="busquedaEvento"),
-    path('buscar3/', buscar3, name="buscar3"),
-    path('listaEventos/', listaEventos, name="listaEventos"),
-    path('eliminarEventos/<int:id>', eliminarEvento, name="eliminarEventos"),
-    path('editarEventos/<int:id>', editar_evento, name="editarEventos"),
 
 
 
--- AppClub/views.py
+++ AppClub/views.py
@@ -5,301 +5,105 @@ from django.views.generic.detail import DetailView
 from django.views.generic.edit import DeleteView, UpdateView, CreateView
 from .models import Socio, Deporte, Evento
 from .forms import *
+from django.urls import reverse_lazy
 # Create your views here.
-
-
-def socios(req, nombre, edad, correo):
-    socios = Socio(nombre=nombre, edad=edad, correo=correo)
-    socios.save()
-
-    return HttpResponse(f"""
-<p>Socio: {socios.nombre} edad: {socios.edad} correo: {socios.correo} Creado con éxito. """)
-
-
-def listar_socios(req):
-    lista = Socio.objects.all()
-
-    return render(req, "lista_socios.html", {"lista_socios": lista})
 
 
 def inicio(req):
     return render(req, "inicio.html")
 
 
-def socio(req):
-    return render(req, "socio.html")
-
-
-def evento(req):
-    return render(req, "evento.html")
-
-
-def deporte(req):
-    return render(req, "deporte.html")
-
-
 class SocioList(ListView):
     model = Socio
-    template_name = "socio_list.html"
-    context_object_name = "socios"
+    template_name = 'socio_list.html'
+    context_object_name = 'socios'
 
 
 class SocioDetail(DetailView):
     model = Socio
-    template_name = "socio_detail.html"
-    context_object_name = "socio"
+    template_name = 'socio_detail.html'
+    context_object_name = 'socio'
 
 
 class SocioCreate(CreateView):
     model = Socio
-    template_name = "socio_create.html"
-    fields = ['nombre', 'apellido', 'edad', 'correo']
-    success_url = '/AppClub/'
+    template_name = 'socio_create.html'
+    fields = '__all__'
+    success_url = reverse_lazy('listarSocios')
 
 
 class SocioUpdate(UpdateView):
     model = Socio
-    template_name = "socio_update.html"
-    fields = ('__all__')
-    success_url = '/AppClub/listaSocios'
+    template_name = 'socio_update.html'
+    fields = '__all__'
+    success_url = reverse_lazy('listarSocios')
 
 
 class SocioDelete(DeleteView):
     model = Socio
-    template_name = "socio_delete.html"
-    success_url = '/AppClub/'
+    template_name = 'socio_delete.html'
+    success_url = reverse_lazy('listarSocios')
 
 
-def socioFormulario(req):
+class DeporteList(ListView):
+    model = Deporte
+    template_name = 'deporte_list.html'
+    context_object_name = 'deportes'
 
-    if req.method == 'POST':
-        miFormulario = SocioFormulario(req.POST)
 
-        if miFormulario.is_valid():
+class DeporteDetail(DetailView):
+    model = Deporte
+    template_name = 'deporte_detail.html'
+    context_object_name = 'deporte'
 
-            data = miFormulario.cleaned_data
 
-            socio = Socio(nombre=data["nombre"], apellido=data["apellido"],
-                          edad=data["edad"], correo=data["correo"])
-            socio.save()
+class DeporteCreate(CreateView):
+    model = Deporte
+    template_name = 'deporte_create.html'
+    fields = '__all__'
+    success_url = reverse_lazy('listarDeportes')
 
-            return render(req, "inicio.html")
 
-    else:
-        miFormulario = SocioFormulario()
-        return render(req, "socioFormulario.html", {"miFormulario": miFormulario})
+class DeporteUpdate(UpdateView):
+    model = Deporte
+    template_name = 'deporte_update.html'
+    fields = '__all__'
+    success_url = reverse_lazy('listarDeportes')
 
 
-def busquedaApellido(req):
-    return render(req, "busquedaApellido.html")
+class DeporteDelete(DeleteView):
+    model = Deporte
+    template_name = 'deporte_delete.html'
+    success_url = reverse_lazy('listarDeportes')
 
 
-def buscar1(req: HttpRequest):
+class EventoList(ListView):
+    model = Evento
+    template_name = 'evento_list.html'
+    context_object_name = 'eventos'
 
-    if req.GET['Apellido']:
-        apellido = req.GET['Apellido']
-        socios = Socio.objects.filter(apellido=apellido)
-        return render(req, "resultadoBusqueda1.html", {"socios": socios})
-    else:
-        return HttpResponse("socio no existente.")
 
+class EventoDetail(DetailView):
+    model = Evento
+    template_name = 'evento_detail.html'
+    context_object_name = 'evento'
 
-def deporteFormulario(req):
-    if req.method == 'POST':
-        miFormulario = DeporteFormulario(req.POST)
 
-        if miFormulario.is_valid():
+class EventoCreate(CreateView):
+    model = Evento
+    template_name = 'evento_Create.html'
+    fields = '__all__'
+    success_url = reverse_lazy('listarEventos')
 
-            data = miFormulario.cleaned_data
-            deporte = Deporte(
-                nombre=data["nombre"], descripcion=data["descripcion"], fecha_inicio=data["fecha_inicio"], fecha_fin=data["fecha_fin"])
-            deporte.save()
-            return render(req, "inicio.html")
-    else:
-        miFormulario = DeporteFormulario()
-        return render(req, "deporteFormulario.html", {"miFormulario": miFormulario})
 
+class EventoUpdate(UpdateView):
+    model = Evento
+    template_name = 'evento_update.html'
+    fields = '__all__'
+    success_url = reverse_lazy('listarEventos')
 
-def busquedaDeporte(req):
-    return render(req, "busquedaDeporte.html")
 
-
-def buscar2(req: HttpRequest):
-
-    if req.GET['nombre']:
-        nombre = req.GET['nombre']
-        deportes = Deporte.objects.filter(nombre__icontains=nombre)
-        return render(req, "resultadoBusqueda2.html", {"deportes": deportes})
-    else:
-        return HttpResponse("Deporte no existente.")
-
-
-def eventoFormulario(req):
-    if req.method == 'POST':
-        miFormulario = EventoFormulario(req.POST)
-
-        if miFormulario.is_valid():
-            data = miFormulario.cleaned_data
-            evento = Evento(titulo=data["titulo"], fecha=data["fecha"]
-                            )
-
-            evento.save()
-
-            return render(req, "inicio.html")
-
-    else:
-        miFormulario = EventoFormulario()
-        return render(req, "eventoFormulario.html", {"miFormulario": miFormulario})
-
-
-def busquedaEvento(req):
-    return render(req, "busquedaEvento.html")
-
-
-def buscar3(req: HttpRequest):
-
-    if req.GET['titulo']:
-        titulo = req.GET['titulo']
-        evento = Evento.objects.get(titulo=titulo)
-        return render(req, "resultadoBusqueda3.html", {"evento": evento})
-    else:
-        return HttpResponse("Evento no existente.")
-
-
-def listaSocios(req):
-
-    socios = Socio.objects.all()
-
-    return render(req, "listaSocios.html", {"socios": socios})
-
-
-def listaDeportes(req):
-
-    deportes = Deporte.objects.all()
-
-    return render(req, "listaDeportes.html", {"deportes": deportes})
-
-
-def listaEventos(req):
-
-    eventos = Evento.objects.all()
-
-    return render(req, "listaEventos.html", {"eventos": eventos})
-
-
-def eliminarSocio(req, id):
-    if req.method == 'POST':
-
-        socio = Socio.objects.get(id=id)
-        socio.delete()
-
-        socios = Socio.objects.all()
-
-        return render(req, "listaSocios.html", {"socios": socios})
-
-
-def eliminarDeporte(req, id):
-    if req.method == 'POST':
-
-        deporte = Deporte.objects.get(id=id)
-        deporte.delete()
-
-        deportes = Deporte.objects.all()
-
-        return render(req, "listaDeportes.html", {"deportes": deportes})
-
-
-def eliminarEvento(req, id):
-
-    if req.method == 'POST':
-
-        evento = Evento.objects.get(id=id)
-        evento.delete()
-
-        eventos = Evento.objects.all()
-
-        return render(req, "listaEventos.html", {"eventos": eventos})
-
-
-def editar_socio(req, id):
-
-    socio = Socio.objects.get(id=id)
-
-    if req.method == 'POST':
-        miFormulario = SocioFormulario(req.POST)
-
-        if miFormulario.is_valid():
-
-            data = miFormulario.cleaned_data
-
-            socio.nombre = data["nombre"]
-            socio.apellido = data["apellido"]
-            socio.edad = data["edad"]
-            socio.correo = data["correo"]
-
-            socio.save()
-
-            return render(req, "inicio.html")
-    else:
-        miFormulario = SocioFormulario(initial={
-            "nombre": socio.nombre,
-            "apellido": socio.apellido,
-            "edad": socio.edad,
-            "correo": socio.correo,
-        })
-
-        return render(req, "editarSocio.html", {"miFormulario": miFormulario, "id": socio.id})
-
-
-def editar_deporte(req, id):
-
-    deporte = Deporte.objects.get(id=id)
-
-    if req.method == 'POST':
-        miFormulario = DeporteFormulario(req.POST)
-
-        if miFormulario.is_valid():
-
-            data = miFormulario.cleaned_data
-
-            deporte.nombre = data["nombre"]
-            deporte.descripcion = data["descripcion"]
-            deporte.fecha_inicio = data["fecha_inicio"]
-            deporte.fecha_fin = data["fecha_fin"]
-            deporte.save()
-
-            return render(req, "inicio.html")
-    else:
-        miFormulario = DeporteFormulario(initial={
-            "nombre": deporte.nombre,
-            "descripcion": deporte.descripcion,
-            "fecha_inicio": deporte.fecha_inicio,
-            "fecha_fin": deporte.fecha_fin,
-        })
-
-        return render(req, "editarDeporte.html", {"miFormulario": miFormulario, "id": deporte.id})
-
-
-def editar_evento(req, id):
-
-    evento = Evento.objects.get(id=id)
-
-    if req.method == 'POST':
-        miFormulario = EventoFormulario(req.POST)
-
-        if miFormulario.is_valid():
-
-            data = miFormulario.cleaned_data
-
-            evento.titulo = data["titulo"]
-            evento.fecha = data["fecha"]
-
-            evento.save()
-
-    else:
-        miFormulario = EventoFormulario(initial={
-            "titulo": evento.titulo,
-            "fecha": evento.fecha,
-        })
-
-        return render(req, "editarEvento.html", {"miFormulario": miFormulario, "id": evento.id})
+class EventoDelete(DeleteView):
+    model = Evento
+    template_name = 'evento_delete.html'
+    success_url = reverse_lazy('listarEventos')
